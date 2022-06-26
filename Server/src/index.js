12caa29822921224cd5099d6a8694caca264bf25
kpps17 <sinhaarchit17@gmail.com>
2022-06-26 12:30:18 +0530

    added middleware setup

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,14 +1,21 @@
 const express = require("express");
+const {notFoundHandler, errorHandler} = require(`./middleware`);
+
+require('dotenv').config();
 
 const app = express();
-const PORT = 8080
+const PORT = 8080 || process.env.PORT;
 
 app.get('/', (req, res) => {
     res.json({
         message: "Hi from express",
-    }).status(201);
+        status: res.statusCode
+    })
 })
 
 app.listen(PORT, () => {
     console.log(`Server lisitening on port ${PORT}`);
 })
+
+app.use(errorHandler);
+app.use(notFoundHandler);
